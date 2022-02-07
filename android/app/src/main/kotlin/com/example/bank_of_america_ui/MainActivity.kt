ab4d3136139548ed0968f89b06e6e59c4b7939a8
Abdulazeez Sami <ponanzar@gmail.com>
2022-02-07 19:56:51 +0200

    boa_ui

---
+++ android/app/src/main/kotlin/com/example/bank_of_america_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bank_of_america_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+List<Color> bgColor = [
+  const Color(0xFF083C6F),
+  const Color(0xFF4DBEE0),
+  const Color(0xFFFFFFFF),
+];
