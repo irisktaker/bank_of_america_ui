4b968ccc06c1c918ec40ea3f3d77a0500ea503af
irisktaker <ponanzar@gmail.com>
2022-02-23 11:39:55 +0200

    utility bills

--- lib/screens/utility_bills/pending_screen.dart
+++ lib/screens/utility_bills/pending_screen.dart
@@ -1,9 +1,16 @@
 import 'package:bank_of_america_ui/widgets/button/custom_main_btn.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class PendingScreen extends StatelessWidget {
+class PendingScreen extends StatefulWidget {
   const PendingScreen({Key? key}) : super(key: key);
 
+  @override
+  State<PendingScreen> createState() => _PendingScreenState();
+}
+
+class _PendingScreenState extends State<PendingScreen> {
+  bool isSwitched = false;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -14,6 +21,7 @@ class PendingScreen extends StatelessWidget {
         children: [
           containerBox(
             size,
+            height: size.height * 0.18,
             icon: "assets/icons/electricity_bill.png",
             bgIconColor: const Color(0xFF4DBEE0),
             title: "Electricity Bill",
@@ -24,24 +32,107 @@ class PendingScreen extends StatelessWidget {
           const SizedBox(height: 10),
           containerBox(
             size,
+            height: size.height * 0.18,
             icon: "assets/icons/water_bill.png",
             bgIconColor: Colors.red[900]!,
             title: "Water Bill",
             amount: "\$ \t48.43",
             btnTextColor: Colors.white,
-            btnColor: Colors.orange[800]!,
+            btnColor: Colors.red[600]!,
+            notificationIcon: Icons.error,
           ),
           const SizedBox(height: 10),
-          Container(
-            width: size.width,
+          containerBox(
+            size,
             height: size.height * 0.38,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
+            icon: "assets/icons/tv.png",
+            bgIconColor: const Color(0xFF4DBEE0),
+            title: "TV Monthly Subscribe",
+            amount: "\$ \t29.99",
+            btnTextColor: Colors.grey,
+            btnColor: Colors.white,
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                monthlySubscriptionsInformation(
+                  title: "Process Date",
+                  child: Text(
+                    "Wed, Nov. 03, 2022",
+                    style: TextStyle(
+                      color: Colors.red[600]!,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+                monthlySubscriptionsInformation(
+                  title: "Process Date",
+                  child: Row(
+                    children: [
+                      Text(
+                        "Master Card **** 3456",
+                        style: TextStyle(
+                          color: Colors.grey[600]!,
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Container(
+                        width: 38,
+                        height: 25,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade300,
+                          image: const DecorationImage(
+                            image: AssetImage(
+                              "assets/images/mastercard-26135.png",
+                            ),
+                            alignment: Alignment.topRight,
+                            scale: 40,
+                          ),
+                        ),
+                      ),
+                      const Icon(Icons.arrow_drop_down),
+                    ],
+                  ),
+                ),
+                monthlySubscriptionsInformation(
+                  title: "Autopay",
+                  child: Transform.scale(
+                    scale: 0.8,
+                    child: CupertinoSwitch(
+                      value: isSwitched,
+                      onChanged: (value) {
+                        setState(() {
+                          isSwitched = value;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Column monthlySubscriptionsInformation({
+    required title,
+    required Widget child,
+  }) {
+    return Column(
+      children: [
+        const SizedBox(height: 7),
+        const Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title),
+            child,
+          ],
+        ),
+      ],
     );
   }
 
@@ -53,10 +144,13 @@ class PendingScreen extends StatelessWidget {
     required String amount,
     required Color btnTextColor,
     required Color btnColor,
+    Widget? child,
+    required double height,
+    IconData? notificationIcon,
   }) {
     return Container(
       width: size.width,
-      height: size.height * 0.18,
+      height: height,
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
       decoration: BoxDecoration(
         color: Colors.white,
@@ -67,8 +161,8 @@ class PendingScreen extends StatelessWidget {
           Row(
             children: [
               Container(
-                width: 55,
-                height: 55,
+                width: 50,
+                height: 50,
                 decoration: BoxDecoration(
                   color: bgIconColor,
                   borderRadius: BorderRadius.circular(35),
@@ -134,23 +228,32 @@ class PendingScreen extends StatelessWidget {
               ),
             ],
           ),
+          SizedBox(
+            child: child,
+          ),
           Expanded(child: Container()),
           CustomMainBtn(
             onTap: () {},
             widget: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
+              children: [
+                const Spacer(flex: 5),
+                const Text(
                   "Details",
                   style: TextStyle(
                     color: Colors.white,
                   ),
                 ),
-                SizedBox(width: 12),
-                Icon(
+                const SizedBox(width: 12),
+                const Icon(
                   Icons.keyboard_arrow_down_outlined,
                   color: Colors.white,
                 ),
+                const Spacer(flex: 3),
+                Icon(
+                  notificationIcon,
+                  color: Colors.red[700],
+                ),
+                const SizedBox(width: 10)
               ],
             ),
           ),
